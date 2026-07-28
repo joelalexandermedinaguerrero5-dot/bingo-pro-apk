b6206c42c9e0704cbed8d22cfb588ac3270bf1ec
joelalexandermedinaguerrero5-dot <joelalexandermedinaguerrero5@gmail.com>
2026-07-27 22:49:27 -0400

    Create capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,17 @@
+const config = {
+  appId: 'com.reinos018.bingopro',
+  appName: 'Bingo PRO',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2500,
+      backgroundColor: '#0f0f1a',
+      androidScaleType: 'CENTER_CROP'
+    }
+  }
+};
+
+module.exports = config;
